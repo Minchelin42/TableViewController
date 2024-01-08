632da317339a19498132f34b643ac2740629d3ef
Minchelin42 <mje0211@naver.com>
2024-01-08 17:33:40 +0900

    Struct 활용

--- TableViewController/TodoTableViewController.swift
+++ TableViewController/TodoTableViewController.swift
@@ -7,34 +7,30 @@
 
 import UIKit
 
+struct Todo {
+    let list: String
+    var check: Bool
+    var star: Bool
+}
+
 class TodoTableViewController: UITableViewController {
-    
-    var list = ["그립톡 구매", "사이다 구매","아이패트 최저가 알아보기", "침대 매트리스 가격 비교"]
-    var check = [false, false, false, false]
-    var star = [false, false, false, false]
 
     @IBOutlet var topInputView: UIView!
     @IBOutlet var inputTextField: UITextField!
     @IBOutlet var plusButton: UIButton!
+    
+    var todoList: [Todo] = [Todo(list: "에어팟 맥스 구매", check: false, star: false),
+                            Todo(list: "아이폰 액정 A/S", check: false, star: true),
+                            Todo(list: "가성비 매트리스 알아보기", check: false, star: false),
+                            Todo(list: "제로콜라 대용량 구매", check: false, star: false)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         navigationItem.title = "쇼핑"
         
-        topInputView.backgroundColor = UIColor(named: "todoGray")
-        topInputView.layer.cornerRadius = 10
-        
-        inputTextField.backgroundColor = .clear
-        inputTextField.borderStyle = .none
-        inputTextField.placeholder = "무엇을 구매하실 예정이신가요?"
-        
-        plusButton.backgroundColor = UIColor(named: "buttonGray")
-        plusButton.setTitle("추가", for: .normal)
-        plusButton.setTitleColor(.black, for: .normal)
-        plusButton.titleLabel?.font = .systemFont(ofSize: 13, weight: .semibold)
-        plusButton.layer.cornerRadius = 10
-
+        inputViewDesign()
+        plusButtonDesign()
     }
     
     @IBAction func inputFinish(_ sender: UITextField) {
@@ -43,9 +39,7 @@ class TodoTableViewController: UITableViewController {
     
     @IBAction func plusButtonTapped(_ sender: UIButton) {
         if inputTextField.text != "" {
-            list.append(inputTextField.text!)
-            star.append(false)
-            check.append(false)
+            todoList.append(Todo(list: inputTextField.text!, check: false, star: false))
             tableView.reloadData()
             inputTextField.text = ""
         } else {
@@ -59,7 +53,7 @@ class TodoTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return list.count
+        return todoList.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -68,9 +62,8 @@ class TodoTableViewController: UITableViewController {
         cell.checkButton.tag = indexPath.row
         cell.starButton.tag = indexPath.row
 
-        
-        var checkImage = check[indexPath.row] ? "checkmark.square.fill" : "checkmark.square"
-        var starImage = star[indexPath.row] ? "star.fill" : "star"
+        var checkImage = todoList[indexPath.row].check ? "checkmark.square.fill" : "checkmark.square"
+        var starImage = todoList[indexPath.row].star ? "star.fill" : "star"
         
         cell.checkButton.setImage(UIImage(systemName: checkImage), for: .normal)
         cell.checkButton.tintColor = .black
@@ -78,27 +71,25 @@ class TodoTableViewController: UITableViewController {
         cell.starButton.setImage(UIImage(systemName: starImage), for: .normal)
         cell.starButton.tintColor = .black
         
-        cell.titleLabel.text = "\(list[indexPath.row])"
+        cell.titleLabel.text = "\(todoList[indexPath.row].list)"
         cell.titleLabel.font = .systemFont(ofSize: 14, weight: .regular)
         
         cell.listView.backgroundColor = UIColor(named: "todoGray")
         cell.listView.layer.cornerRadius = 10
         
         cell.starButton.addTarget(self, action: #selector(starButtonTapped), for: .touchUpInside)
-        
         cell.checkButton.addTarget(self, action: #selector(checkButtonTapped), for: .touchUpInside)
-        
         
         return cell
     }
 
     @objc func starButtonTapped(sender: UIButton){
-        star[sender.tag].toggle()
+        todoList[sender.tag].star.toggle()
         tableView.reloadRows(at: [IndexPath(row: sender.tag, section: 0)], with: .fade)
     }
     
     @objc func checkButtonTapped(sender: UIButton){
-        check[sender.tag].toggle()
+        todoList[sender.tag].check.toggle()
         tableView.reloadRows(at: [IndexPath(row: sender.tag, section: 0)], with: .fade)
     }
     
@@ -112,11 +103,27 @@ class TodoTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            list.remove(at: indexPath.row)
-            star.remove(at: indexPath.row)
-            check.remove(at: indexPath.row)
+            todoList.remove(at: indexPath.row)
             tableView.reloadData()
         }
     }
+    
+    func inputViewDesign() {
+        topInputView.backgroundColor = UIColor(named: "todoGray")
+        topInputView.layer.cornerRadius = 10
+        
+        inputTextField.backgroundColor = .clear
+        inputTextField.borderStyle = .none
+        inputTextField.placeholder = "무엇을 구매하실 예정이신가요?"
+    }
+    
+    func plusButtonDesign() {
+        plusButton.backgroundColor = UIColor(named: "buttonGray")
+        plusButton.setTitle("추가", for: .normal)
+        plusButton.setTitleColor(.black, for: .normal)
+        plusButton.titleLabel?.font = .systemFont(ofSize: 13, weight: .semibold)
+        plusButton.layer.cornerRadius = 10
+    }
+    
 
 }
